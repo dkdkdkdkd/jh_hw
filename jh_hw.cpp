2255dfdbb8813585b963654489756d1c98a6f88d
GNU <GNU@DESKTOP-QSM7CFJ>
2019-04-17 13:19:24 +0900

    commit 2

--- jh_hw.cpp
+++ jh_hw.cpp
@@ -3,10 +3,17 @@
 
 #include "pch.h"
 #include <iostream>
+#include<stdio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int a, b;
+
+	printf("hello world");
+	scanf("%d%d", &a, &b);
+
+	printf("%d",a+b);
+
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
